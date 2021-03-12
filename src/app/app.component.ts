a924d5abde9f7db66f10acc3cefd25962c1a9aad
João Mateus Games Coelho <mateusgamescoelho547@gmail.com>
2021-03-12 17:45:55 -0300

    Criando e estudando primeira aplicacao em Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'first-angular';
+  title = 'teste';
+
+  cars = ["Gol", "Palio", "Fusca", "Ferrari"];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { FullBannerComponent } from './shared/full-banner/full-banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FullBannerComponent,
   ],
   imports: [
     BrowserModule,
